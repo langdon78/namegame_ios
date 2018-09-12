4b57dcd6a35fbe876a65466ade0ebd4bd2ab458a
James Langdon <jlangdon@switchmatehome.com>
2018-09-12 02:19:33 -0400

    Add NetworkManager reference and profiles properties

--- NameGame/NameGame/NameGame.swift
+++ NameGame/NameGame/NameGame.swift
@@ -9,17 +9,54 @@
 import Foundation
 
 protocol NameGameDelegate: class {
+    func refresh()
 }
 
 class NameGame {
+    var allProfiles: [Profile] = [] {
+        didSet {
+            delegate?.refresh()
+        }
+    }
+    
+    var visibleProfiles: [Profile] {
+        guard allProfiles.count > 5 else { return [] }
+        return Array(allProfiles[0...5])
+    }
+
+    var networkManager: NetworkManager
 
     weak var delegate: NameGameDelegate?
 
     let numberPeople = 6
-
+    
+    init(networkManager: NetworkManager = NetworkManager.shared, delegate: NameGameDelegate? = nil) {
+        self.networkManager = networkManager
+        self.delegate = delegate
+        loadGameData { [weak self] in
+            self?.delegate?.refresh()
+        }
+    }
+    
+    func profile(for id: Int) -> Profile? {
+        guard visibleProfiles.count >= id else { return nil }
+        return visibleProfiles[id]
+    }
 
     // Load JSON data from API
     func loadGameData(completion: @escaping () -> Void) {
-
+        networkManager.items(at: Endpoint.profile.url) { [weak self] (result: Result<[Profile]>) in
+            switch result {
+            case .success(let data):
+                self?.allProfiles = data
+            case .failure(let error):
+                print(error)
+            }
+            completion()
+        }
+    }
+    
+    func shuffle() {
+        
     }
 }
