0a10edc89eef6f1b58d9b46a9b4f6b298a88eb6e
James Langdon <jlangdon@switchmatehome.com>
2018-09-12 01:00:17 -0400

    Refactor NetworkManager response parsing

--- NameGame/NameGame/NetworkManager.swift
+++ NameGame/NameGame/NetworkManager.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2018 WillowTree Apps. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+typealias NetworkResponse<T> = (Result<T>) -> Void
 
 enum Result<T> {
     case success(T)
@@ -15,6 +17,7 @@ enum Result<T> {
 
 enum ResponseError: Error {
     case missingData
+    case unableToParseResponse
 }
 
 enum HTTPRequestMethod: String {
@@ -30,11 +33,59 @@ struct HTTPRequest {
     }
 }
 
+protocol Castable {
+    init?(data: Data)
+}
+
+extension UIImage: Castable {}
+
 final class NetworkManager {
     static let shared: NetworkManager = NetworkManager()
+    let session = URLSession(configuration: URLSessionConfiguration.default)
     
-    func send<T: Codable>(urlRequest: URLRequest, completionHandler: @escaping (Result<[T]>) -> ()) {
-        let session = URLSession(configuration: URLSessionConfiguration.default)
+    func items<T>(for urlRequest: URLRequest, completionHandler: @escaping NetworkResponse<[T]>) where T: Codable {
+        send(urlRequest: urlRequest) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let entities: [T] = self?.entities(for: data) else {
+                    completionHandler(.failure(ResponseError.unableToParseResponse))
+                    return
+                }
+                completionHandler(.success(entities))
+            case .failure(let error): completionHandler(.failure(error))
+            }
+        }
+    }
+    
+    func blob<T>(for urlRequest: URLRequest, completionHandler: @escaping NetworkResponse<T>) where T: Castable {
+        send(urlRequest: urlRequest) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let parsed: T = self?.entity(for: data) else {
+                    completionHandler(.failure(ResponseError.unableToParseResponse))
+                    return
+                }
+                completionHandler(.success(parsed))
+            case .failure(let error): completionHandler(.failure(error))
+            }
+        }
+    }
+    
+    private func entity<T>(for data: Data) -> T? where T: Castable {
+        return T.init(data: data)
+    }
+    
+    private func entities<T>(for data: Data) -> [T]? where T: Codable {
+        do {
+            let decoded = try JSONDecoder().decode([T].self, from: data)
+            return decoded
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    private func send(urlRequest: URLRequest, completionHandler: @escaping NetworkResponse<Data>) {
         session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
             if let networkError = error {
                 completionHandler(.failure(networkError))
@@ -44,19 +95,7 @@ final class NetworkManager {
                 completionHandler(.failure(ResponseError.missingData))
                 return
             }
-            do {
-                let responseData = try JSONDecoder().decode([T].self, from: data)
-                completionHandler(.success(responseData))
-            } catch {
-                print("An error occured parsing response data: \(error)")
-            }
+            completionHandler(.success(data))
         }).resume()
-    }
-    
-    static func request(for request: HTTPRequest) -> URLRequest {
-        var urlRequest = URLRequest(url: request.url)
-        urlRequest.httpMethod = request.requestMethod.rawValue
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        return urlRequest
     }
 }
