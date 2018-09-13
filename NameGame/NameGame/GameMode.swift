428578fdfa49b013624c733f6aa6e20deb8a2142
James Langdon <jlangdon@switchmatehome.com>
2018-09-13 09:18:53 -0400

    Add GameMode model

---
+++ NameGame/NameGame/GameMode.swift
@@ -0,0 +1,17 @@
+//
+//  GameType.swift
+//  NameGame
+//
+//  Created by James Langdon on 9/13/18.
+//  Copyright © 2018 WillowTree Apps. All rights reserved.
+//
+
+import Foundation
+
+enum GameMode {
+    case normal
+    case mattMode
+    case reverseMode
+    case hintMode
+    case teamMode
+}
